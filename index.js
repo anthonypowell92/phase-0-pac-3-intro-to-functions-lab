5a8049369c38fe95c56adea8ef7a2170a91ceadd
AnthonyPowell <anthonydewpow@gmail.com>
2022-11-24 20:04:26 -0600

    commit tree 607cbfc7dc2f8d53376907b699c8d48852756196

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  console.log ("hello");
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log ("HELLO");
+  return string.toUpperCase();
+}
+function logWhisper(string) {
+  console.log ("hello");
+  return string.toLowerCase();
+}
+function sayHiToHeadphonedRoommate(string) {
+  console.log ("hello");
+  return string.toLowerCase();
+}
